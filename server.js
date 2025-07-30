7703d1bc668b73f5cb38234223ce632678b94f07
TONYSTARKOP <msonu2018@gmail.com>
2025-07-30 11:06:03 +0530

    initial commit

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const PORT = process.env.PORT || 3000;
-const CLIENT_ORIGIN = process.env.CLIENT_ORIGIN || 'https://cinewave-frontend-gules.vercel.app';
+const CLIENT_ORIGIN = process.env.CLIENT_ORIGIN || 'https://cine-wave-murex.vercel.app';
 const app = express();
 
 // Allow your React app origin
